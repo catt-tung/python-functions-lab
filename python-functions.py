50abdd7b8fd1deff3b2373e6731b848898604a78
Catherine Tung <catt.tung@gmail.com>
2022-04-06 13:38:47 -0400

    solution challenge 3

--- python-functions.py
+++ python-functions.py
@@ -14,4 +14,8 @@ print(largest([1, 2, 3, 4, 0]))
 
 #Challenge 3: Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
 
+def occurrences(str1, str2):
+  return str1.count(str2)
+
+print(occurrences('fleep floop', 'p'))
 #Challenge 4: Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
