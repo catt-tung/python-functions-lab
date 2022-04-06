9e804d0d2d349a232f2abb0325adb33df9c477b4
Catherine Tung <catt.tung@gmail.com>
2022-04-06 18:20:52 -0400

    solution to 1

--- python-functions.py
+++ python-functions.py
@@ -1,5 +1,9 @@
 #Challenge 1: Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+def sum_to(n):
+  nums = list(range(n+1))
+  return sum(nums)
 
+print(sum_to(6))
 
 #Challenge 2: Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 def largest(list):
